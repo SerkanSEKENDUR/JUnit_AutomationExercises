7d67a88c8a37df134c202bca0f5ab6be5e4afe82
SerkanSEKENDUR <serkan.sekendur@gmail.com>
2023-03-04 00:19:15 +0300

    S1

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,24 @@
+package utilities;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public abstract class TestBase {
+    // abstract yapmamizin sebebi, bu class'dan obje uretilmesinin onune gecmektir. Sart degildir
+    protected WebDriver driver;
+    @Before
+    public void setUp() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @After
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
